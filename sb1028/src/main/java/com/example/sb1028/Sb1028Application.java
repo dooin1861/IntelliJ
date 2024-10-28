5d2a2affd75fb4d654480e7c2c5552490a83be96
dooin1861 <dooin1861@naver.com>
2024-10-28 19:31:41 +0900

    asd

---
+++ sb1028/src/main/java/com/example/sb1028/Sb1028Application.java
@@ -0,0 +1,13 @@
+package com.example.sb1028;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1028Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb1028Application.class, args);
+	}
+
+}
---
+++ sb1028/src/test/java/com/example/sb1028/Sb1028ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb1028;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb1028ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
