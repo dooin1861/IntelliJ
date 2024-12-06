a56b8f186e220af9c769a354bdd1daf96bb7c6ea
dooin1861 <dooin1861@naver.com>
2024-12-06 15:45:26 +0900

    ChatServer1206_3

--- sb1128/src/main/java/com/example/sb1128/Sb1128Application.java
+++ sb1128/src/main/java/com/example/sb1128/Sb1128Application.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class Sb1128Application {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(Sb1128Application.class, args);
     }
 }
